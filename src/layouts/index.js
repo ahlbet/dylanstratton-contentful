579ae6b113a212c7d99c39907d1524c40f157245
Dylan Stratton <dm.stratton94@gmail.com>
2018-03-26 13:41:57 -0500

    fix file paths for sass

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,8 +3,8 @@ import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
 
 import Header from '../components/Header';
-// import './index.scss';
-import '../assets/sass/main.sass';
+import './index.scss';
+// import '../assets/sass/main.sass';
 
 const TemplateWrapper = ({ children }) => (
   <div>
