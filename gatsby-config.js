84efa81a2a28a2efe94c553c1ccbab9a4733c99b
Dylan Stratton <dm.stratton94@gmail.com>
2019-02-11 15:57:10 -0600

    gatsby image implementation

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,9 +12,25 @@ module.exports = {
         accessToken: process.env.CONTENTFUL_ACCESS_TOKEN || ''
       }
     },
+    // {
+    //   resolve: `gatsby-source-filesystem`,
+    //   options: {
+    //     name: `data`,
+    //     path: `${__dirname}/src/data/`
+    //   }
+    // },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `img`,
+        path: `${__dirname}/src/assets/img/`
+      }
+    },
     'gatsby-plugin-react-helmet',
     `gatsby-transformer-remark`,
     `gatsby-plugin-sass`,
-    `gatsby-plugin-netlify`
+    `gatsby-plugin-netlify`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
   ]
 };
--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -1,23 +1,12 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import Img from 'gatsby-image'
+import { graphql } from 'gatsby'
 
 import Footer from '../components/Footer'
 import Layout from '../components/layout'
 
-import gallery1 from "../assets/img/gallery1.png";
-import gallery3 from "../assets/img/gallery3.png";
-import gallery4 from "../assets/img/gallery4.png";
-import gallery5 from "../assets/img/gallery5.png";
-import gallery6 from "../assets/img/gallery6.png";
-import gallery7 from "../assets/img/gallery7.png";
-import gallery9 from "../assets/img/gallery9.png";
-import gallery10 from "../assets/img/gallery10.png";
-import gallery11 from "../assets/img/gallery11.jpg";
-import gallery12 from "../assets/img/gallery12.png";
-import gallery13 from "../assets/img/gallery13.png";
-import gallery14 from "../assets/img/gallery14.jpg";
-
-const SecondPage = () => (
+const SecondPage = ({ data }) => (
   <Layout>
 
     <div className="gallery__heading">
@@ -25,43 +14,94 @@ const SecondPage = () => (
 
       <div className="u-center-text gallery__heading--links">
         <Link className="gallery__heading--links-home" to="/">Home</Link>
-        <Link className="gallery__heading--links-blog" to="blog">Blog</Link>
+        <Link className="gallery__heading--links-blog" to="/blog">Blog</Link>
       </div>
     </div>
 
     <div className="row">
-      <img className="col-1-of-2" src={gallery14} alt="Gallery" />
-      <img className="col-1-of-2" src={gallery13} alt="Gallery" />
+      <Img fixed={data.gallery14.childImageSharp.fixed} className="col-1-of-2" />
+      <Img fixed={data.gallery13.childImageSharp.fixed} className="col-1-of-2" />
     </div>
 
     <div className="row">
-      <img className="col-1-of-2" src={gallery1} alt="Gallery" />
-      <img className="col-1-of-2" src={gallery12} alt="Gallery" />
+      <Img fixed={data.gallery1.childImageSharp.fixed} className="col-1-of-2" />
+      <Img fixed={data.gallery12.childImageSharp.fixed} className="col-1-of-2" />
     </div>
 
     <div className="row">
-      <img className="col-1-of-2" src={gallery3} alt="Gallery" />
-      <img className="col-1-of-2" src={gallery4} alt="Gallery" />
+      <Img fixed={data.gallery3.childImageSharp.fixed} className="col-1-of-2" />
+      <Img fixed={data.gallery4.childImageSharp.fixed} className="col-1-of-2" />
     </div>
 
     <div className="row">
-      <img className="col-1-of-2" src={gallery5} alt="Gallery" />
-      <img className="col-1-of-2" src={gallery6} alt="Gallery" />
+      <Img fixed={data.gallery5.childImageSharp.fixed} className="col-1-of-2" />
+      <Img fixed={data.gallery6.childImageSharp.fixed} className="col-1-of-2" />
     </div>
 
     <div className="row">
-      <img className="col-1-of-2" src={gallery7} alt="Gallery" />
-      <img className="col-1-of-2" src={gallery11} alt="Gallery" />
+      <Img fixed={data.gallery7.childImageSharp.fixed} className="col-1-of-2" />
+      <Img fixed={data.gallery11.childImageSharp.fixed} className="col-1-of-2" />
     </div>
 
     <div className="row">
-      <img className="col-1-of-2" src={gallery9} alt="Gallery" />
-      <img className="col-1-of-2" src={gallery10} alt="Gallery" />
+      <Img fixed={data.gallery9.childImageSharp.fixed} className="col-1-of-2" />
+      <Img fixed={data.gallery10.childImageSharp.fixed} className="col-1-of-2" />
     </div>
 
 
     <Footer />
   </Layout>
 )
+
+export const fixedImage = graphql`
+  fragment fixedImage on File {
+    childImageSharp {
+      fixed(width: 550, height: 550) {
+        ...GatsbyImageSharpFixed
+      }
+    }
+  }
+`
+
+export const query = graphql`
+  query {
+    gallery14: file(relativePath: { eq: "gallery14.jpg" }) {
+      ...fixedImage
+    },
+    gallery13: file(relativePath: { eq: "gallery13.png" }) {
+      ...fixedImage
+    },
+    gallery1: file(relativePath: { eq: "gallery1.png" }) {
+      ...fixedImage
+    },
+    gallery12: file(relativePath: { eq: "gallery12.png" }) {
+      ...fixedImage
+    },
+    gallery3: file(relativePath: { eq: "gallery3.png" }) {
+      ...fixedImage
+    },
+    gallery4: file(relativePath: { eq: "gallery4.png" }) {
+      ...fixedImage
+    },
+    gallery5: file(relativePath: { eq: "gallery5.png" }) {
+      ...fixedImage
+    },
+    gallery6: file(relativePath: { eq: "gallery6.png" }) {
+      ...fixedImage
+    },
+    gallery7: file(relativePath: { eq: "gallery7.png" }) {
+      ...fixedImage
+    },
+    gallery11: file(relativePath: { eq: "gallery11.jpg" }) {
+      ...fixedImage
+    },
+    gallery9: file(relativePath: { eq: "gallery9.png" }) {
+      ...fixedImage
+    },
+    gallery10: file(relativePath: { eq: "gallery10.png" }) {
+      ...fixedImage
+    },
+  }
+  `
 
 export default SecondPage
